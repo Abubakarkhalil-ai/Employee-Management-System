e5515d504eb8d607b8bc420bc0e05b016ac103b6
safiullah22814 <SI533@live.mdx.ac.uk>
2026-04-03 17:05:33 +0500

    Add files via upload

---
+++ EmployeeBST.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+namespace EmployeeManagementSystem
+{
+    public class EmployeeBST
+    {
+        private EmployeeNode root;
+
+        public EmployeeBST()
+        {
+            root = null;
+        }
+    }
+}
