2f510b684afa835e452e28536538789427ef5cdd
safiullah22814 <SI533@live.mdx.ac.uk>
2026-04-04 19:32:41 +0500

    Add files via upload

--- EmployeeBST.cs
+++ EmployeeBST.cs
@@ -11,5 +11,25 @@ namespace EmployeeManagementSystem
         {
             root = null;
         }
+
+        public void Insert(Employee employee)
+        {
+            root = InsertRecursive(root, employee);
+        }
+
+        private EmployeeNode InsertRecursive(EmployeeNode node, Employee employee)
+        {
+            if (node == null)
+                return new EmployeeNode(employee);
+
+            if (employee.Id < node.Data.Id)
+                node.Left = InsertRecursive(node.Left, employee);
+            else if (employee.Id > node.Data.Id)
+                node.Right = InsertRecursive(node.Right, employee);
+            else
+                Console.WriteLine("Employee ID already exists.");
+
+            return node;
+        }
     }
 }
