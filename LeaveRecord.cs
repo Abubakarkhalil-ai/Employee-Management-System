8700dce01b259e4fdd245d2521a9d34e60021c34
remon0007 <ahmedhanjala007@gmail.com>
2026-04-06 19:03:58 +0100

    Add files via upload

---
+++ LeaveRecord.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace EmployeeManagementSystem
+{
+    public class LeaveRecord
+    {
+        public string LeaveType { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public string Reason { get; set; }
+
+        public LeaveRecord(string leaveType, string startDate, string endDate, string reason)
+        {
+            LeaveType = leaveType;
+            StartDate = startDate;
+            EndDate = endDate;
+            Reason = reason;
+        }
+    }
+}
